0ef027302869bb90ab9986317387e541ed76cc69
amarcinkowski <andrzej.marcinkowski@gmail.com>
2016-10-22 11:18:18 +0200

    YABOR (yet again a bit of refactoring)

--- src/main/java/io/github/amarcinkowski/utils/StringUtils.java
+++
@@ -1,9 +0,0 @@
-package io.github.amarcinkowski.utils;
-
-public class StringUtils {
-
-	public final static String capitalizeFirstLetter(String str) {
-		return str.substring(0, 1).toUpperCase() + str.substring(1);
-	}
-
-}
