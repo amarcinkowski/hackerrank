fac28a487fde97ac8223e47eb2482220d21f716f
amarcinkowski <andrzej.marcinkowski@gmail.com>
2016-11-06 00:44:08 +0100

    +1 solved

--- hackerrank-solutions/src/main/java/io/github/amarcinkowski/hackerrank/javaintroduction/JavaIntToString.java
+++ hackerrank-solutions/src/main/java/io/github/amarcinkowski/hackerrank/javaintroduction/JavaIntToString.java
@@ -5,20 +5,28 @@ import io.github.amarcinkowski.solutionframework.Solution;
 import io.github.amarcinkowski.solutionframework.TestInfo;
 
 public class JavaIntToString extends Solution {
-public JavaIntToString(TestInfo ti) {super(ti);}	/*
-* overloaded log:
-
-	private static void log(String msg) {}
-/* */
+	public JavaIntToString(TestInfo ti) {
+		super(ti);
+	} /*
+		 * overloaded log:
+		 * 
+		 * private static void log(String msg) {} /*
+		 */
 
 	Scanner scanner;
 
 	public void execute() {
 		log("execute");
 		scanner = new Scanner(System.in);
-		int x = scanner.nextInt();
+		int n = scanner.nextInt();
+		scanner.close();
+		String s = String.format("%d", n);
+		log(n == Integer.parseInt(s));
+		System.out.println(s);
 	}
 
-// hr:
-	public static void main(String[] args) {new Solution().execute();}
+	// hr:
+	public static void main(String[] args) {
+		new Solution().execute();
+	}
 }
