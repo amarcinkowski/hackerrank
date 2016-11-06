386de38d24b731e68ef2fe01ec061a3eaa0367d8
amarcinkowski <andrzej.marcinkowski@gmail.com>
2016-11-06 17:25:19 +0100

    fix: no digits at the class & package start

--- solution-core/src/main/java/io/github/amarcinkowski/utils/StringUtils.java
+++ solution-core/src/main/java/io/github/amarcinkowski/utils/StringUtils.java
@@ -5,20 +5,20 @@ import java.text.Normalizer;
 import com.google.common.base.CaseFormat;
 
 public class StringUtils {
-	
+
 	private static final String NONASCII = "[^\\x00-\\x7F]";
 
 	public static String normalize(String string) {
 		string = Normalizer.normalize(string, Normalizer.Form.NFD);
 		return string.replaceAll(NONASCII, "").replaceAll("[ ]", "");
 	}
-	
+
 	public static String packagify(String string) {
-		return normalize(string).replaceAll("\\-", "").toLowerCase();
+		return normalize(string).replaceAll("\\-", "").replaceAll("^([0-9\\-]+)", "").toLowerCase();
 	}
-	
+
 	public static String camelify(String string) {
-		string = string.replaceAll(" ", "-").replaceAll(NONASCII, "");
+		string = string.replaceAll(" ", "-").replaceAll("^([0-9\\-]+)", "").replaceAll(NONASCII, "");
 		return CaseFormat.LOWER_HYPHEN.to(CaseFormat.UPPER_CAMEL, string);
 	}
 
