78f0f7ea240cdc7437839577ce691acaaf282a7c
amarcinkowski <andrzej.marcinkowski@gmail.com>
2016-11-09 23:31:20 +0100

    rename rfctr

--- codility-solutions/src/test/java/io/github/amarcinkowski/codility/java/tests/TimeComplexity.java
+++ codility-solutions/src/test/java/io/github/amarcinkowski/codility/java/tests/TimeComplexity.java
@@ -5,15 +5,8 @@ import org.junit.Test;
 import io.github.amarcinkowski.solutionframework.SolutionTestSuite;
 import io.github.amarcinkowski.solutionframework.TestInfo;
 
-public class Time_complexity extends SolutionTestSuite {
+public class TimeComplexity extends SolutionTestSuite {
 
-/* example test:
-	@Test
-	@TestInfo(platform = "", domain = "", subdomain = "", classname = "", taskDescription = "")
-	public void giveMeAName() {
-		runTest();
-	}
-*/
 	@Test
 	@TestInfo(platform = "codility", domain = "java", subdomain = "3-time_complexity", classname = "Tapeequilibrium", taskDescription = "TapeEquilibrium")
 	public void tapeequilibrium() {
